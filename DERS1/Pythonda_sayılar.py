4c68bbe5f64fdb103d558bedc47a29689545c0d7
enes muzaffer gümüş <enesmuzaffergumus@gmail.com>
2024-07-28 20:49:11 +0300

    Pythonda sayılar konusu işlendi.pythonda_sayılar dosyası oluşturuldu.

---
+++ DERS1/Pythonda_sayılar.py
@@ -0,0 +1,97 @@
+# enes="Ögeday Han Türk"
+
+# for i in range(0,10):
+#     print(enes)
+    
+
+# i =10
+# # print(i)
+# print(i**i)
+
+# i=15
+# print(i)6
+# a=46
+# b=45
+# c=a+2*b
+# print(a,b,c)
+
+# pi_sayısı=3.14
+
+# çap=9
+
+# çevre=pi_sayısı*çap
+
+# print(çevre)
+
+#Pythonda bir değişkenin değerini değiştirmek için basit bir yöntem bulunmaktadır.
+
+# a=2
+# b=3
+
+# a_geçici=a
+# a=b
+# b=a_geçici
+
+# print(f"a={a},b={b}")
+
+
+#Diğer yöntem: (Daha kısadır.)
+
+# a=2
+# b=3
+# a,b=b,a
+
+# print(f"a={a},b={b}")
+
+
+#Değişkenin değerini arttırma:
+
+# a=10
+# a=a+1
+# print(a)
+
+#Değişkenin değerini arttırmada diğer yöntem:
+# a=10
+# a+=5
+# print(a)
+
+
+
+# a=10
+# a=a-5
+# print(a)
+
+
+# a=10
+# a-=5
+# print(a)
+
+
+# a=100
+# a=a*2
+# print(a)
+
+# a=100
+# a*=2
+# print(a)
+
+
+# a=100
+# a/=5
+# print(a)
+
+
+# a=100
+# a=a/5
+# print(a)
+
+"""
+
+
+Çoklu Yorum Satırı
+
+#Bu kodlar Ögedayhan tarafından yazmıştır.
+
+
+
+"""
