18e24d0541e35cfbd466d2916ae8c798923f2e9b
enes muzaffer gümüş <enesmuzaffergumus@gmail.com>
2024-07-28 22:21:14 +0300

    matematik operatörleri konusu tamamlandı.matematik_operatörleri dosyası oluşturuldu.

---
+++ matematik_operatörleri.py
@@ -0,0 +1,58 @@
+# x = 1.1
+# y =2.5
+# print(x+y)
+
+# a=45
+# b=54
+# c=98
+
+# print(a-b-c)
+# print(10//4)#bu // operatörü bölümün tam kısmını verir. bölüm==2
+# print(10/4)# bu / operatörü bölümün  tamamını  verir. bölüm==2.5
+# print(10%2)# bu % operatörü kalanı verir kalan ==0
+
+# a=4
+# if a%2==0:
+#     print("Sayımız çifttir.")
+# else:
+#     print("Sayımız tektir.")
+
+# baslangıc=int(input("başlangıç değeri: "))
+# bitis=int(input("bitiş değeri: "))
+# for i in range(baslangıc,bitis):
+#     if not i%2==0:
+#         print(i)
+
+# print(8/3)
+
+# print(round(8/3,4))
+
+
+# baslangıç=int(input("başlangıç sayısı: "))
+# bitiş=int(input("bitiş sayısı: "))
+# for i in range(baslangıç,bitiş):
+#     if i% 2==0:
+#         print(i)
+
+# print(2**3)
+
+# print(16**0.5)
+
+
+
+#İşaret Değiştirme:
+# a=8
+# print(-a)
+
+
+# a=-10
+# print(a+10)
+
+
+#Operatörleri beraber kullanma ve işlem sırası: 
+# 1.Parantez içi
+# 2.Çarpma ve Bölme
+# 3. Toplama ve Çıkarma
+#İşlmeler soldan sağa göre değerlendirilir.
+
+print(8+4*3/2-18)
